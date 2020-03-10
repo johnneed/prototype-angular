1ba87ac1a9fc32873bde81ba2034670938e6bc18
John Need <johnneed@johnneed.com>
2020-03-10 15:25:28 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {SearchComponent} from './search/search.component';
+import {ReportsComponent} from './reports/reports.component';
+import {ReportDetailsComponent} from './report-details/report-details.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'reports', component: ReportsComponent},
+  {path: 'report-details', component: ReportDetailsComponent},
+  {path: '**', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {SearchComponent} from './search/search.component';
+import {ReportsComponent} from './reports/reports.component';
+import {ReportDetailsComponent} from './report-details/report-details.component';
+import {MainLayoutComponent} from './main-layout/main-layout.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    DashboardComponent,
+    SearchComponent,
+    ReportsComponent,
+    ReportDetailsComponent,
+    MainLayoutComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
